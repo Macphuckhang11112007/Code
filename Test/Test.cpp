eaeea492eb0059cb6753a1e4d77bb80aae13afe3
Macphuckhang11112007 <macphuckhang11112007@gmail.com>
2025-07-01 13:25:11 +0700

    Refactor C++ project configuration and update main program output

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,8 +1,16 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+int main()
+{
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
+    for (const string& word : msg)
+    {
+        cout << word << " ";
+    }
+    cout << endl;
 }
