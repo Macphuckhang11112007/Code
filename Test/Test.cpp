a7dcf315ef73af396730e48cae0f8acf0ba80fb0
Mạc Phúc Khang <macphuckhang11112007@gmail.com>
2025-07-04 23:03:13 +0700

    Update

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,16 +1,9 @@
-#include <iostream>
-#include <vector>
-#include <string>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main()
+int main ()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
+    cout << "Hello World!" << endl;
+    return 0;
 }
