b4016fe849cddaa37c8e297cb4bde0300f054897
Mạc Phúc Khang <macphuckhang11112007@gmail.com>
2025-08-14 05:00:42 +0700

    ...
    Signed-off-by: Mạc Phúc Khang <macphuckhang11112007@gmail.com>

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -2,54 +2,65 @@
 
 #include <bits/stdc++.h>
 
-#define pii pair<int, int>
-#define fi first
-#define se second
-
 using namespace std;
 
-const int kMaxN = 1e5 + 5;
+const int N = 1e5 + 5;
 
-int                                 n, spf[kMaxN];
-long long                           ans;
-vector<int>                         primes;
-unordered_map<long long, long long> cnt;
+struct Node
+{
+    int       par, val, cnt, maxx;
+    long long sum;
+} a[N];
+
+int n, m, q;
+
+int Par(int u)
+{
+    if (u == a[u].par) { return u; }
+    a[u].par = Par(a[u].par);
+    return a[u].par;
+}
+
+void DSU(int u, int v)
+{
+    int pu = Par(u);
+    int pv = Par(v);
+    if (pu == pv) { return; }
+    if (a[pu].cnt < a[pv].cnt) { swap(pu, pv); }
+    a[pv].par = pu;
+    a[pu].cnt += a[pv].cnt;
+    a[pu].sum += a[pv].sum;
+    a[pu].maxx = max(a[pu].maxx, a[pv].maxx);
+    return;
+}
 
 int main()
 {
     cin.tie(0)->sync_with_stdio(0);
-    cin >> n;
-    spf[0] = spf[1] = 0;
-    for (int i = 2; i <= n; ++i)
-    {
-        if (!spf[i])
-        {
-            spf[i] = i;
-            primes.push_back(i);
-        }
-        for (const int &j : primes)
-        {
-            if (1LL * i * j > n || j > spf[i]) { break; }
-            spf[i * j] = j;
-        }
-    }
+    cin >> n >> m;
     for (int i = 1; i <= n; ++i)
     {
-        unordered_map<int, int> fact;
-        int                     x = i;
-        while (spf[x])
-        {
-            ++fact[spf[x]];
-            x /= spf[x];
-        }
-        long long y = 1;
-        for (const auto &[num, c] : fact)
-        {
-            if (c & 1) { y *= num; }
-        }
-        ans += 2 * cnt[y] + 1;
-        ++cnt[y];
+        int x;
+        cin >> x;
+        a[i].par  = i;
+        a[i].val  = x;
+        a[i].cnt  = 1;
+        a[i].sum  = x;
+        a[i].maxx = x;
     }
-    cout << ans;
+    while (m--)
+    {
+        int u, v;
+        cin >> u >> v;
+        DSU(u, v);
+    }
+    cin >> q;
+    while (q--)
+    {
+        int u;
+        cin >> u;
+        int pu = Par(u);
+        cout << a[pu].cnt << " " << a[pu].sum << " " << a[pu].maxx << "\n";
+    }
     return 0;
 }
