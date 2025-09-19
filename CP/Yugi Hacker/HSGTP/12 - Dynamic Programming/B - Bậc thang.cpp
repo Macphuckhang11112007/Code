121521d310249c971f49be6da2a2219ebebd701e
Macphuckhang11112007 <macphuckhang11112007@gmail.com>
2025-09-20 01:17:26 +0700

    Last Sync: 20-09-2025 01:17:25 (Mobile)

--- CP/Yugi Hacker/HSGTP/12 - Dynamic Programming/B - Bậc thang.cpp
+++ CP/Yugi Hacker/HSGTP/12 - Dynamic Programming/B - Bậc thang.cpp
@@ -16,7 +16,7 @@ int main() {
         cin >> x;
         a[x] = 1;
     }
-    dp[0] = 1;
+    dp[1] = 1;
     for (int i = 2; i <= n; i++) {
         if (a[i]) {
             dp[i] = (1LL * dp[i - 1] + dp[i - 2]) % MOD;
